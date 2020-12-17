f609c8423bd42650276402b02871d049803a8f5c
ELISHA1994 <elishabello2014@gmail.com>
2020-12-17 23:22:50 +0100

    Step3

---
+++ src/AddNewItem.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+import { AddItemButton } from "./styles"
+import {NewItemForm} from "./NewItemForm";
+
+interface AddNewItemProps {
+    onAdd(text: string): void
+    toggleButtonText: string
+    dark?: boolean
+}
+
+export const AddNewItem = (props: AddNewItemProps) => {
+    const [showForm, setShowForm] = useState(false)
+    const { onAdd, toggleButtonText, dark } = props;
+
+    if (showForm) {
+        // we show ite creation form here
+        return (
+            <NewItemForm
+                onAdd={text => {
+                    onAdd(text)
+                    setShowForm(false)
+                }}
+            />
+        )
+    }
+
+    // return <button onClick={() => setShowForm(true)}>{toggleButtonText}</button>
+    return (
+        <AddItemButton dark={dark} onClick={() => setShowForm(true)}>
+            {toggleButtonText}
+        </AddItemButton>
+    )
+}
